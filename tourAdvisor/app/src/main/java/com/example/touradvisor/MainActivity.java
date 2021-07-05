a39ff0a335524af30ef883a925965706a5e69c91
laurentmanuel <lm.laurentmanuel@gmail.com>
2021-07-05 16:15:23 +0200

    init_master
    Base projet

---
+++ tourAdvisor/app/src/main/java/com/example/touradvisor/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.touradvisor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private final static int MENU_ID_MAPS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_ID_MAPS, 0,"Maps");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==MENU_ID_MAPS) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
