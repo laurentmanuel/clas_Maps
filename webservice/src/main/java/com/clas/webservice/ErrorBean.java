783cc9f9f9df7f7e9718f52ed805f53e12fd790a
laurentmanuel <lm.laurentmanuel@gmail.com>
2021-07-07 11:21:28 +0200

    try & catch à compléter

---
+++ webservice/src/main/java/com/clas/webservice/ErrorBean.java
@@ -0,0 +1,17 @@
+package com.clas.webservice;
+
+public class ErrorBean {
+    private String message;
+
+    public ErrorBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- webservice/src/main/java/com/clas/webservice/MyRestController.java
+++ webservice/src/main/java/com/clas/webservice/MyRestController.java
@@ -6,24 +6,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
 public class MyRestController {
+    private int COORDONNEES_INCORRECTES = 518;
 
     @Autowired
     private PointDao pointDao;
 
-    //http://localhost:8080/testPoint
+    //http://localhost:8889/testPoint
     @GetMapping("/testPoint")
     public String testPoint(){
         System.out.println("/testPoint");
-        return "Hello World !";
+        return "Hello World!";
     }
 
     //récupérer la position des points
-    //http://localhost:8080/getPoints
+    //http://localhost:8889/getPoints
     @PostMapping("/getPoints")
     public List<PointBean> getPoints(){
         List<PointBean> pointlist = pointDao.findAll();
@@ -33,9 +34,24 @@ public class MyRestController {
     //envoyer la position des points
     //http://localhost:8080/sendPoint
     @PostMapping("/sendPoint")
-    public void sendPoint(@RequestBody PointBean pointBean){
-        //controle
-        System.out.println("/sendPoint : lat > " + pointBean.getLat_point() + " & lon > " + pointBean.getLon_point());
-        pointDao.save(pointBean);
+    public ErrorBean sendPoint(@RequestBody PointBean pointBean, HttpServletResponse response) {
+        try {
+            if ((pointBean.getLat_point() = null || pointBean.getLon_point() != null) && ()){
+                sendPoint();
+            }
+            throw new Exception("Coordonnées hors limites");
+
+            //controle
+            System.out.println("/sendPoint : lat > " + pointBean.getLat_point() + " & lon > " + pointBean.getLon_point());
+            pointDao.save(pointBean);
+
+            return null;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            response.setStatus(COORDONNEES_INCORRECTES);
+            return new ErrorBean("Erreur : " + e.getMessage());
+        }
+
     }
 }
